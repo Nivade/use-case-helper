9127edc6d6617667d307de1ed2bca4fc8032d29b
Nivade <nhavandeursen@gmail.com>
2016-02-22 10:02:24 +0100

    Add .gitignore file

---
+++ use-case-helper/ContentCollection.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uch
+{
+    public class ContentCollection
+    {
+
+        private List<Drawable> drawables = new List<Drawable>(); 
+
+        public ContentCollection()
+        {
+            
+        }
+    }
+}
---
+++ use-case-helper/Drawables/Actor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uch.Drawables
+{
+    public class Actor : Drawable
+    {
+
+        public Actor(string name) : base(name)
+        {
+            Name = name;
+        }
+
+    }
+}
