03b325ec555f5ac4f144d054ea3e5e01a969b50d
Nivade <nhavandeursen@gmail.com>
2016-02-23 17:02:51 +0100

    finish base layout design

--- use-case-helper/Program.cs
+++ use-case-helper/Program.cs
@@ -16,7 +16,7 @@ namespace use_case_helper
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new fMain());
         }
     }
 }
