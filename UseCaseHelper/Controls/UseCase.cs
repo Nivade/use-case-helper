bc35134f70d63afb737e635dcf0c2a21295cbc38
Niels van Deursen <nhavandeursen@gmail.com>
2016-09-05 20:47:05 +0200

    add use case control

---
+++ UseCaseHelper/Controls/UseCase.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UseCaseHelper.Controls
+{
+    public partial class UseCase : Element
+    {
+        public UseCase()
+        {
+            InitializeComponent();
+        }
+
+        public UseCase(Diagram diagram) : this()
+        {
+            this.diagram = diagram;
+        }
+    }
+}
